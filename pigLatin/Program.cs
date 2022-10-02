55fb321571e6d641da8e6f21d3042cdb08a49b9e
muritz <mauricechisom275@gmail.com>
2022-10-02 16:00:57 +0100

    pig latin task complete

---
+++ pigLatin/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter text: ");
+            string input = Console.ReadLine();
+
+            string[] separator = { " " };
+            string[] strList = input.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            Console.Write("Pig latin translation: ");
+            foreach (string str in strList)
+            {
+                char firstLetter = str[0];
+                string data = str.Substring(1)+firstLetter+"ay";
+                Console.Write("{0} ", data);
+            }
+
+        }
+    }
+}
