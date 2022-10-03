6761e947ee302f71699dbe2a84281dab2c7078ad
muritz <mauricechisom275@gmail.com>
2022-10-03 01:46:26 +0100

    fix

--- GuessGame/Program.cs
+++ GuessGame/Program.cs
@@ -6,22 +6,13 @@ namespace GuessGame
     {
         static void Main(string[] args)
         {
-            /*
-             *  0. use the random lib to get a random integer between 0-100
-                1. You get the input of the user
-                2. You create a variable that is initially 0. This is the number of tries\
-                3. You run a while loop that continuses till guess == random
-                4. Inside the while loop theres a condition.. If the guess is > than the random, tell the person it's greater
-                    if the guess is less than the random, tell the person it's less else declare the person winner and break the loop.
-                5. N:B at the start of the while loop, increment the number of tries by 1.. 
-             */
 
             Random random = new Random();
             int randNum = random.Next(10);
             int retries = 5;
 
             Console.WriteLine("===============================");
-            Console.WriteLine("  Welcome to the Guessing game ");
+            Console.WriteLine(" Welcome to the Guessing game ");
             Console.WriteLine("===============================");
 
             do
@@ -48,7 +39,20 @@ namespace GuessGame
                     if (Int32.Parse(usersGuess) == randNum)
                     {
                         Console.WriteLine("Correct, you guessed right!");
-                        break;
+                        Console.Write("Continue playing? Y/N: ");
+                        string answer = Console.ReadLine();
+                        Console.WriteLine("--------------------------------");
+
+                        if (answer.ToLower() == "y" || answer.ToLower() == "yes")
+                        {
+                            randNum = random.Next(10);
+                            retries = 5;
+                            continue;
+                        }
+                        else
+                        {
+                            break;
+                        }
                     } else if (Int32.Parse(usersGuess) > randNum)
                     {
                         Console.WriteLine("Incorrect!! Number is too large");
@@ -66,12 +70,26 @@ namespace GuessGame
                         Console.WriteLine("--------------------------------");
                         break;
                     }
-                } else
+                } 
+                else
                 {
                     Console.WriteLine("--------------------------------");
                     Console.WriteLine("You've run out of retries");
+                    Console.Write("Continue playing? Y/N: ");
+                    string answer = Console.ReadLine();
                     Console.WriteLine("--------------------------------");
-                    break ;
+
+                    if(answer.ToLower() == "y" || answer.ToLower() == "yes")
+                    {
+                        randNum = random.Next(10);
+                        retries = 5;
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
                 }
             }
             while (true);
